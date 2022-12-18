53e1c18065decc2a512887be2aad6c6632007bce
Dheeraj Kumar <dheeraj.fullstack@gmail.com>
2022-12-18 19:31:58 +0530

    package addition

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # HelloAzB2C
 Azure b2c custom policies
+
+dotnet add package Microsoft.AspNetCore.Authentication.JwtBearer
+dotnet add package Microsoft.AspNetCore.Authentication.OpenIdConnect
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+dotnet add package Microsoft.EntityFrameworkCore.Tools
+dotnet add package System.Text.Json
+dotnet add package 
+dotnet add package 
+dotnet add package 
--- app/src/AzB2CWebAppMVC/Extensions/SessionExtensions.cs
+++ app/src/AzB2CWebAppMVC/Extensions/SessionExtensions.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
-using Newtonsoft.Json;
+using System.Text.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,12 +16,12 @@ namespace AzureADB2CWeb.Extensions
             {
                 return default(T);
             }
-            return JsonConvert.DeserializeObject<T>(data);
+            return JsonSerializer.Deserialize<T>(data);
         }
 
         public static void SetComplexData(this ISession session, string key, object value)
         {
-            session.SetString(key, JsonConvert.SerializeObject(value));
+            session.SetString(key, JsonSerializer.Serialize(value));
         }
     }
 }
